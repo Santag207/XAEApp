424ac25ef555e7736e1df1f1ed45671a8f853f33
Santiago Castro Zuluaga <castrozsantiago@javeriana.edu.co>
2024-12-07 16:55:01 -0500

    Commit 2

--- lib/item_list_screen.dart
+++ lib/item_list_screen.dart
@@ -1,37 +1,278 @@
 import 'package:flutter/material.dart';
 
-class ItemListScreen extends StatelessWidget {
+class ItemListScreen extends StatefulWidget {
+  @override
+  _ItemListScreenState createState() => _ItemListScreenState();
+}
+
+class _ItemListScreenState extends State<ItemListScreen> {
   final List<Map<String, dynamic>> dummyScannedItems = [
     {'nombre': 'Pieza 1', 'cantidad': 10},
     {'nombre': 'Pieza 2', 'cantidad': 5},
+    {'nombre': 'Pieza 3', 'cantidad': 7},
   ];
+
+  final List<Map<String, dynamic>> dummyRegisteredItems = [
+    {'nombre': 'Pieza 1', 'codigo': 'PZ001', 'apartadoPor': 'Juan Pérez', 'fecha': DateTime(2024, 12, 10), 'cantidad': 2},
+    {'nombre': 'Pieza 2', 'codigo': 'PZ002', 'apartadoPor': 'María López', 'fecha': DateTime(2024, 12, 12), 'cantidad': 1},
+    {'nombre': 'Pieza 3', 'codigo': 'PZ003', 'apartadoPor': 'Juan Pérez', 'fecha': DateTime(2024, 12, 13), 'cantidad': 3},
+  ];
+
+  String searchQueryLista = ''; // Nueva variable para el buscador de "Lista"
+  String searchQuery = ''; // Variable existente para "Registrados"
+  String? selectedIntegrante;
+  bool isViewingRegistered = false;
 
   @override
   Widget build(BuildContext context) {
+    // Filtrar piezas por nombre en lista
+    final filteredScannedItems = dummyScannedItems
+        .where((item) => item['nombre'].toLowerCase().contains(searchQueryLista.toLowerCase()))
+        .toList();
+
+    // Filtrar piezas por nombre en registradas
+    final filteredRegisteredItems = dummyRegisteredItems
+        .where((item) =>
+    (selectedIntegrante == null || item['apartadoPor'] == selectedIntegrante) &&
+        item['nombre'].toLowerCase().contains(searchQuery.toLowerCase()))
+        .toList();
+
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text('Ítems Escaneados'),
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.black,
+        elevation: 0,
+        title: Padding(
+          padding: EdgeInsets.only(top: 30.0), // Ajusta el padding superior
+          child: Text(
+            'Gestión de Ítems',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: dummyScannedItems.length,
-        itemBuilder: (context, index) {
-          final item = dummyScannedItems[index];
-          return ListTile(
-            title: Text(item['nombre']),
-            subtitle: Text('Cantidad: ${item['cantidad']}'),
-          );
-        },
+      body: Padding(
+        padding: EdgeInsets.only(top: 20.0, left: 16.0, right: 16.0),
+        child: Column(
+          children: [
+            // Botones de Lista y Registrados
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _buildToggleButton('Lista', false),
+                _buildToggleButton('Registrados', true),
+              ],
+            ),
+            SizedBox(height: 20),
+
+            // Mostrar el buscador exclusivo para "Lista"
+            if (!isViewingRegistered)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: 'Buscar por nombre de pieza...',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    filled: true,
+                    fillColor: Colors.grey[800],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                    prefixIcon: Icon(Icons.search, color: Colors.white),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      searchQueryLista = value; // Actualizar búsqueda de "Lista"
+                    });
+                  },
+                ),
+              ),
+
+            // Mostrar buscador y dropdown exclusivo para "Registrados"
+            if (isViewingRegistered)
+              Row(
+                children: [
+                  // Buscador de texto para "Registrados"
+                  Expanded(
+                    child: TextField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: 'Buscar por nombre...',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        filled: true,
+                        fillColor: Colors.grey[800],
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0)),
+                        contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          searchQuery = value;
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(width: 10),
+
+                  // Dropdown para seleccionar integrante
+                  DropdownButton<String>(
+                    value: selectedIntegrante,
+                    hint: Text('Integrante', style: TextStyle(color: Colors.grey)),
+                    dropdownColor: Colors.black,
+                    style: TextStyle(color: Colors.white),
+                    items: [
+                      ...dummyRegisteredItems
+                          .map((item) => item['apartadoPor'])
+                          .toSet()
+                          .map((integrante) {
+                        return DropdownMenuItem<String>(
+                          value: integrante,
+                          child: Text(integrante),
+                        );
+                      }).toList()
+                    ],
+                    onChanged: (value) {
+                      setState(() {
+                        selectedIntegrante = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            SizedBox(height: 20),
+
+            // Lista de ítems
+            Expanded(
+              child: isViewingRegistered
+                  ? ListView.builder(
+                itemCount: filteredRegisteredItems.length,
+                itemBuilder: (context, index) {
+                  final item = filteredRegisteredItems[index];
+                  return ListTile(
+                    title: Text(
+                      '${item['nombre']} (${item['codigo']})',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      '${item['apartadoPor']}',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    onTap: () => _showRegisteredItemDetails(context, item),
+                  );
+                },
+              )
+                  : ListView.builder(
+                itemCount: filteredScannedItems.length,
+                itemBuilder: (context, index) {
+                  final item = filteredScannedItems[index];
+                  return ListTile(
+                    title: Text(item['nombre'], style: TextStyle(color: Colors.white)),
+                    subtitle: Text(
+                      'Cantidad disponible: ${item['cantidad']}',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    onTap: () => _showAvailableItemDetails(context, item),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('Simulación: Lista confirmada')),
-          );
-        },
-        child: Text('Apartar'),
-        backgroundColor: Colors.green,
+    );
+  }
+
+  // Botón de alternancia (Lista o Registrados)
+  Widget _buildToggleButton(String text, bool viewRegistered) {
+    final isSelected = isViewingRegistered == viewRegistered;
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: isSelected ? Colors.white : Colors.red, // Cambiar el color del botón activo
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
       ),
+      onPressed: () {
+        setState(() {
+          isViewingRegistered = viewRegistered;
+          searchQuery = '';
+          selectedIntegrante = null;
+        });
+      },
+      child: Text(
+        text,
+        style: TextStyle(color: isSelected ? Colors.black : Colors.white), // Cambiar el color del texto
+      ),
+    );
+  }
+
+  // Mostrar detalles de un ítem disponible
+  void _showAvailableItemDetails(BuildContext context, Map<String, dynamic> item) {
+    // Calcular piezas ocupadas basándose en el nombre
+    final int piezasOcupadas = dummyRegisteredItems
+        .where((e) => e['nombre'] == item['nombre']) // Buscar coincidencias por nombre
+        .fold<int>(0, (sum, e) => sum + (e['cantidad'] as int)); // Sumar cantidades ocupadas y convertir a int
+
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.black,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
+      ),
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Detalles del Ítem Disponible',
+                style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              SizedBox(height: 10),
+              Text('Nombre: ${item['nombre']}', style: TextStyle(color: Colors.white)),
+              Text('Código: ${item['nombre'].toUpperCase()}_COD', // Código de ejemplo
+                  style: TextStyle(color: Colors.white)),
+              Text('Cantidad disponible: ${item['cantidad']}', style: TextStyle(color: Colors.white)),
+              Text('Piezas ocupadas: $piezasOcupadas', style: TextStyle(color: Colors.white)),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  // Mostrar detalles de un ítem registrado
+  void _showRegisteredItemDetails(BuildContext context, Map<String, dynamic> item) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.black,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
+      ),
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Detalles del Ítem Registrado', style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
+              SizedBox(height: 10),
+              Text('Nombre: ${item['nombre']}', style: TextStyle(color: Colors.white)),
+              Text('Código: ${item['codigo']}', style: TextStyle(color: Colors.white)),
+              Text('Apartado por: ${item['apartadoPor']}', style: TextStyle(color: Colors.white)),
+              Text('Fecha de apartado: ${item['fecha']}', style: TextStyle(color: Colors.white)),
+              Text('Cantidad apartada: ${item['cantidad']}', style: TextStyle(color: Colors.white)),
+            ],
+          ),
+        );
+      },
     );
   }
 }
--- lib/menu_screen.dart
+++ lib/menu_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'home_screen.dart';
 import 'qr_scanner_screen.dart';
 import 'profile_screen.dart';
-import 'reserved_items_screen.dart';
+import 'item_list_screen.dart';
+import 'pending_screen.dart';
 
 class MenuScreen extends StatefulWidget {
   @override
@@ -15,44 +16,90 @@ class _MenuScreenState extends State<MenuScreen> {
   // Lista de pantallas con la pantalla de inicio como primera opción
   final List<Widget> _screens = [
     HomeScreen(),         // Pantalla de inicio
+    PendingScreen(),      // Pantalla de pendientes
     QRScannerScreen(),    // Pantalla de escaneo QR
+    ItemListScreen(),    // Pantalla de piezas
     ProfileScreen(),      // Pantalla de perfil
-    ReservedItemsScreen() // Pantalla de piezas
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screens[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        backgroundColor: Colors.black, // Fondo negro del menú
-        selectedItemColor: Colors.red, // Color rojo para el ítem seleccionado
-        unselectedItemColor: Colors.black, // Color negro para ítems no seleccionados
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Inicio',
+      bottomNavigationBar: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _currentIndex,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.grey,
+            backgroundColor: Colors.black,
+            onTap: (index) {
+              if (index != 2) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              }
+            },
+            items: [
+              _buildAnimatedItem(Icons.home, 'Inicio', 0),
+              _buildAnimatedItem(Icons.list_alt, 'Pendientes', 1),
+              BottomNavigationBarItem(
+                icon: SizedBox.shrink(), // Espacio vacío para el botón QR
+                label: '',
+              ),
+              _buildAnimatedItem(Icons.widgets_outlined, 'Piezas', 3),
+              _buildAnimatedItem(Icons.person, 'Perfil', 4),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.qr_code),
-            label: 'Escanear QR',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Perfil',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'Piezas',
+          Positioned(
+            bottom: 5, // Ajustar la posición del botón QR
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _currentIndex = 2; // El botón central navega a Escanear QR
+                });
+              },
+              child: Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12), // Esquinas redondeadas
+                  border: Border.all(color: Colors.black, width: 3),
+                ),
+                child: Icon(
+                  Icons.qr_code, // Ícono QR
+                  size: 28, // Tamaño del ícono
+                  color: Colors.black,
+                ),
+              ),
+            ),
           ),
         ],
       ),
+    );
+  }
+
+  /// Método para crear los ítems animados
+  BottomNavigationBarItem _buildAnimatedItem(IconData icon, String label, int index) {
+    return BottomNavigationBarItem(
+      icon: TweenAnimationBuilder(
+        tween: Tween<double>(begin: 1.0, end: _currentIndex == index ? 1.2 : 1.0),
+        duration: Duration(milliseconds: 200),
+        curve: Curves.easeInOut,
+        builder: (context, scale, child) {
+          return Transform.scale(
+            scale: scale,
+            child: Icon(
+              icon,
+              color: _currentIndex == index ? Colors.white : Colors.grey,
+            ),
+          );
+        },
+      ),
+      label: label,
     );
   }
 }
